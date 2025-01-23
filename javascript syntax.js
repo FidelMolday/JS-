4988c9da5890639086dbdd3022380c04cde585f0
Molday Developer <126654078+FidelMolday@users.noreply.github.com>
2025-01-23 13:57:48 +0300

    Add files via upload

---
+++ javascript syntax.js
@@ -0,0 +1,16 @@
+//variable declaration
+let num = 5; //assigning number 5 to variable named num
+document.write(num);
+
+//javascript is case-sensitive
+let numb = 5; //assigning number 5 to variable
+let Numb =6;
+
+let sum = numb + Numb;
+document.write(sum);
+
+
+//comments
+const summation = x + /*lets add the value y to x*/ y;
+
+
