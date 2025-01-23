c6d42d37320672e5fd75703e13c76388372ac45f
Molday Developer <126654078+FidelMolday@users.noreply.github.com>
2025-01-23 13:58:18 +0300

    Add files via upload

---
+++ javascriptvariables.js
@@ -0,0 +1,15 @@
+//const variables
+const city = "Mumbai";
+
+//this will print a error causing the script not to work
+const cities = "London";
+cities = "Abuja";
+document.write(city);
+
+//changing value
+let message = "I love Faith";
+message = "I love HTML";
+
+document.write(message);
+
+//variables are uniquely identified by identifiers eg letters
